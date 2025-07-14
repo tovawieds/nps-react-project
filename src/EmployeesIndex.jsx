41dbca84d822fc71ac299cd39c5189b1f01fed99
tovawieds <wiedermanntova@gmail.com>
2025-07-14 18:25:35 -0400

    filter by day functionality

--- src/EmployeesIndex.jsx
+++ src/EmployeesIndex.jsx
@@ -1,21 +1,35 @@
+import { useState } from "react";
 import employees from "./employees.json";
 
 export function EmployeesIndex() {
+  const [sortOption, setSortOption] = useState("");
+
   return (
     <div>
-      {employees.map((employee) => (
-        <div key={employee.id}>
-          <h3>
-            {employee.first_name} {employee.last_name}
-          </h3>
-          <p>{employee.email}</p>
-          <ul>
-            {employee.workdays.map((day, index) => (
-              <li key={index}>{day}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      Filter by Day:
+      <select value={sortOption} onChange={(event) => setSortOption(event.target.value)}>
+        <option value="">No Filter</option>
+        <option value="Monday">Monday</option>
+        <option value="Tuesday">Tuesday</option>
+        <option value="Wednesday">Wednesday</option>
+        <option value="Thursday">Thursday</option>
+        <option value="Friday">Friday</option>
+      </select>
+      {employees
+        .filter((employee) => sortOption === "" || employee.workdays.includes(sortOption))
+        .map((employee) => (
+          <div key={employee.id}>
+            <h3>
+              {employee.first_name} {employee.last_name}
+            </h3>
+            <p>{employee.email}</p>
+            <ul>
+              {employee.workdays.map((day, index) => (
+                <li key={index}>{day}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 }
